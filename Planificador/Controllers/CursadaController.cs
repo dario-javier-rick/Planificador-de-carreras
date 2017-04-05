d7bf5178b846715522799ce8059dc910ca98725c
Dario Javier Rick <dario_rick@outlook.com>
2017-04-05 20:12:52 -0300

    Commit

---
+++ Planificador/Controllers/CursadaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planificador.Controllers
+{
+    public class CursadaController : Controller
+    {
+        public ActionResult Index()
+        {
+			List<Models.Materia> lista = new List<Models.Materia>();
+			Models.Materia m = new Models.Materia();
+
+			m.IdMateria = 1;
+			m.Nombre = "Proyecto profesional 2";
+
+			lista.Add(m);
+
+            return View (lista);
+        }
+    }
+}
