9a36b0ccf97e267c00eb55e527eb31f5c0b514c4
Darío Javier Rick <dario_rick@outlook.com>
2017-05-24 13:36:38 -0300

    Pruebas

--- Planificador/BLL/Constantes/Constantes.cs
+++ Planificador/BLL/Constantes/Constantes.cs
@@ -1,18 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Web;
-
-namespace Planificador.BLL.Constantes
-{
-    public class Constantes
-    {
-        public const string Aprobado = "Aprobado";
-
-        //public static readonly string DataManagerPath = AppDomain.CurrentDomain.BaseDirectory;            
-        public static readonly string DataManagerPath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + @"\Data";
-        //ConfigurationManager.AppSettings["DataManagerPath"];
-    }
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Configuration;
+
+using System.IO;
+
+using System.Linq;
+
+using System.Web;
+
+
+
+namespace Planificador.BLL.Constantes
+
+{
+
+    public class Constantes
+
+    {
+
+        public const string Aprobado = "Aprobado";
+
+
+
+        //public static readonly string DataManagerPath = AppDomain.CurrentDomain.BaseDirectory;            
+
+        public static readonly string DataManagerPath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + @"\Data";
+
+        //ConfigurationManager.AppSettings["DataManagerPath"];
+
+    }
+
 }
